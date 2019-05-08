88ba7f1f72249411768fd30d03be26a246f783c4
QA <QA@172.16.16.175>
2019-05-08 12:19:53 +0300

    adding Test Suite

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>12559cf3-bff3-458f-8719-fcb85dfc4f59</testSuiteGuid>
+</TestSuiteEntity>
