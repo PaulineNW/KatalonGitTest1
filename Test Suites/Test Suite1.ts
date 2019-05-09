4e07c669afb7be8e3dcec90ee6545178b8ca585a
QA <QA@172.16.16.175>
2019-05-09 12:02:48 +0300

    commit

--- Test Suites/Test Suite1.ts
+++ Test Suites/Test Suite1.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>12559cf3-bff3-458f-8719-fcb85dfc4f59</testSuiteGuid>
+   <testCaseLink>
+      <guid>87509c20-38fa-48e8-bdf4-4b2e4e53ddf4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/List of Products- Merlin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b37afff-a094-4f0f-b125-7d74b3cf6ad6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific product- Merlin</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
